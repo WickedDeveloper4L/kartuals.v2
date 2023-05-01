395af204cbeb69421c4a29743f1d90b178e30eb7
Ahia Kleine Chris <wickeddeveloper4l@gmail.com>
2023-05-01 18:28:20 +0100

    added mobile nav bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,42 +1,48 @@
-import React from 'react'
-import './header.scss'
-import {Link} from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux/es/exports'
-import CartIcon from '../cart-icon/CartIcon'
-import CartDropdown from '../cart-dropdown/CartDropdown'
-import { selectCartHidden } from '../../redux/cart/cart.selectors'
-import { selectCurrentUser } from '../../redux/user/user.selectors'
-import { signOutStart } from '../../redux/user/user.actions'
-import {RiTShirt2Fill} from 'react-icons/ri'
+import React from "react";
+import "./header.scss";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux/es/exports";
+import CartIcon from "../cart-icon/CartIcon";
+import CartDropdown from "../cart-dropdown/CartDropdown";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
+import { signOutStart } from "../../redux/user/user.actions";
+import TemporaryDrawer from "../utilities/Drawer";
 
 const Header = () => {
-  const currentUser = useSelector(selectCurrentUser)
-  const hidden = useSelector(selectCartHidden)
+  const currentUser = useSelector(selectCurrentUser);
+  const hidden = useSelector(selectCartHidden);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   return (
-    <div className='header'>
-        <Link to="/" className='logo-container'>
-            <RiTShirt2Fill className='logo'/>
-            <span>Kartuals</span>
+    <div className="header">
+      <Link to="/" className="logo-container">
+        <span>KARTUALS</span>
+      </Link>
+      <div className="options">
+        <Link to="/shop" className="option">
+          <h3 className="link">SHOP</h3>
         </Link>
-        <div className="options">
-        <Link to="/shop" className='option'>
-            <h3 className="link">SHOP</h3>
+        <Link to="/shop" className="option">
+          <h3 className="link">CONTACT</h3>
         </Link>
-        <Link to="/shop" className='option'>
-            <h3 className="link">CONTACT</h3>
-        </Link>
-        {currentUser ?
-            <div className='option' onClick={() => dispatch(signOutStart())}>SIGN OUT</div>
-            :
-            <Link className='option' to='/signIn'>SIGN IN</Link>
-        }
-        <CartIcon/>
+        {currentUser ? (
+          <div className="option" onClick={() => dispatch(signOutStart())}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className="option" to="/signIn">
+            SIGN IN
+          </Link>
+        )}
+        <CartIcon />
+        <div className="mobile_drawer">
+          <TemporaryDrawer anchor />
         </div>
-        {hidden? null : <CartDropdown/>}
+      </div>
+      {hidden ? null : <CartDropdown />}
     </div>
-  )
-}
-  
-  export default Header
+  );
+};
+
+export default Header;
