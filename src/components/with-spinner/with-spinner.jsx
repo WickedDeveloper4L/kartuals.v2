de107f6fc5905f6ced8a835e4abe74f4c778ccad
Ahia Kleine Chris <wickeddeveloper4l@gmail.com>
2023-05-04 18:46:10 +0100

    changed the loader types

--- src/components/with-spinner/with-spinner.jsx
+++ src/components/with-spinner/with-spinner.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import { SpinnerContainer, SpinnerOverlay } from './with-spinner.style'
+import React from "react";
+import { PropagateLoader } from "react-spinners";
+import { SpinnerOverlay } from "./with-spinner.style";
 
-const WithSpinner = WrappedComponent =>({isLoading, ...otherProps})=>{
-   return isLoading ? (
-    <SpinnerOverlay>
-      <SpinnerContainer/>
-    </SpinnerOverlay>
-   ): <WrappedComponent {...otherProps}/>
-} 
+const WithSpinner =
+  (WrappedComponent) =>
+  ({ isLoading, ...otherProps }) => {
+    return isLoading ? (
+      <SpinnerOverlay>
+        <PropagateLoader color="#b87333" className="loader" />
+      </SpinnerOverlay>
+    ) : (
+      <WrappedComponent {...otherProps} />
+    );
+  };
 
-export default WithSpinner
+export default WithSpinner;
