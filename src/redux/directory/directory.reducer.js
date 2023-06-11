3668f15a8f76e5d33ecd2c901112e7f641c0eb42
Ahia Kleine Chris <wickeddeveloper4l@gmail.com>
2023-06-11 13:18:16 +0100

    new updates

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -2,32 +2,32 @@ const INITIAL_STATE = {
     section: [
         {
           title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          imageUrl: 'https://i.ibb.co/NrwZNq1/hats.jpg',
           id: 1,
           linkUrl: 'shop/hats'
         },
         {
           title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          imageUrl: 'https://i.ibb.co/4TQ092q/jackets.jpg',
           id: 2,
           linkUrl: 'shop/jackets'
         },
         {
           title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          imageUrl: 'https://i.ibb.co/yQsVxdD/shoes.jpg',
           id: 3,
           linkUrl: 'shop/sneakers'
         },
         {
           title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          imageUrl: 'https://i.ibb.co/xLV5L8m/womensclothing.jpg',
           size: 'large',
           id: 4,
           linkUrl: 'shop/womens'
         },
         {
           title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          imageUrl: 'https://i.ibb.co/7WXhVPV/mens.jpg',
           size: 'large',
           id: 5,
           linkUrl: 'shop/mens'
