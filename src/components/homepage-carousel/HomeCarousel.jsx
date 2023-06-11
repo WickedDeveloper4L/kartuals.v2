294f64ed6c70f57781aa1b2eeb104405f9aebf8d
Ahia Kleine Chris <wickeddeveloper4l@gmail.com>
2023-06-11 13:25:13 +0100

    new pictures for home carousel

--- src/components/homepage-carousel/HomeCarousel.jsx
+++ src/components/homepage-carousel/HomeCarousel.jsx
@@ -11,7 +11,7 @@ const HomeCarousel = () => {
     {
       title: "Seasonal men's clothings",
       subtitle: "Class, touch, maturity, what else can you ask for a man?",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+      imageUrl: "https://i.ibb.co/7WXhVPV/mens.jpg",
       size: "large",
       id: 5,
       linkUrl: "shop/mens",
@@ -20,7 +20,7 @@ const HomeCarousel = () => {
       title: "Super cute women's wears",
       subtitle:
         "Elegance isn't solely defined by what you wear but by how you wear them.",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+      imageUrl: "https://i.ibb.co/xLV5L8m/womensclothing.jpg",
       size: "large",
       id: 4,
       linkUrl: "shop/womens",
@@ -28,21 +28,21 @@ const HomeCarousel = () => {
     {
       title: "hats",
       subtitle: "Classic, Retro, Seasonal, we've got you covered No CAP.",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      imageUrl: "https://i.ibb.co/NrwZNq1/hats.jpg",
       id: 1,
       linkUrl: "shop/hats",
     },
     {
       title: "jackets",
       subtitle: "Custom jackets, trimmed to show your class.",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+      imageUrl: "https://i.ibb.co/4TQ092q/jackets.jpg",
       id: 2,
       linkUrl: "shop/jackets",
     },
     {
       title: "sneakers & slides",
       subtitle: "Slip in our sneakers, this is not a toosie slide.",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+      imageUrl: "https://i.ibb.co/yQsVxdD/shoes.jpg",
       id: 3,
       linkUrl: "shop/sneakers",
     },
